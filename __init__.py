62b367a5e2f1c106c2cd9956aae81e94bc706e7a
Mayank Jha <jhamayank159@gmail.com>
2021-02-18 19:42:41 +0530

    added entry exit points

--- __init__.py
+++ __init__.py
@@ -2,12 +2,26 @@ from flask import Flask,jsonify, make_response,request
 from functions import make_data
 
 app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return "Hello :) This is my API"
 
-@app.route('/dataendpoint', methods=['GET', 'POST'])
+
+@app.route('/dataexitpoint', methods=['GET', 'POST'])
 def makefulldata():
-    username=request.args.get("username")
-    data = make_data(username)
-    return make_response(jsonify(data), 200)
+    try:
+        username=request.args.get("username")
+        data = make_data(username)
+        return make_response(jsonify(data), 200)
+    except:
+        return "Please send a request Parameter "
+
+@app.route('/dataentrypoint', methods=['GET', 'POST'])
+def getfulldata():
+    try:
+       pass
+    except:
+        return "Please send a request Parameter "
 
 if __name__ == '__main__':
     app.debug = True
