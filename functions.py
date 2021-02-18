a9242ba259563a91e59875198e51558775901df7
Mayank Jha <jhamayank159@gmail.com>
2021-02-18 17:19:26 +0530

    added database

--- functions.py
+++ functions.py
@@ -1,10 +1,21 @@
+import mysql.connector
 
 def make_data(username):
     params = dict()
-    params['username'] = username
-    params['heartfailpercent'] = "DummyData"
-    params['heartfailsevierity'] = "DummyData"
+    (params['username'],params['heartfailpercent'],params['heartfailsevierity'])=databasefetcher(username)
     return params
 
 def databasefetcher(username):
-    pass
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="baka",
+        database="healthcare"
+    )
+    cursor = db.cursor()
+    sql = "SELECT * FROM outputtable WHERE username = %s"
+    usr = (username, )
+
+    cursor.execute(sql, usr)
+    result = cursor.fetchall()
+    return result[0]
