60b8446343e02c3ae90a2b4dd8abc3faad22c58e
Mayank Jha <jhamayank159@gmail.com>
2021-02-18 21:45:25 +0530

    data entry and exit point working

--- __init__.py
+++ __init__.py
@@ -1,27 +1,30 @@
-from flask import Flask,jsonify, make_response,request
-from functions import make_data
+from flask import Flask,jsonify, make_response,request,render_template
+from functions import make_data, databaseentry;
 
 app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def home():
     return "Hello :) This is my API"
 
 
-@app.route('/dataexitpoint', methods=['GET', 'POST'])
+@app.route('/dataexitpoint', methods=['GET'])
 def makefulldata():
     try:
         username=request.args.get("username")
         data = make_data(username)
         return make_response(jsonify(data), 200)
     except:
-        return "Please send a request Parameter "
+        return "Please send a request Parameter"
 
-@app.route('/dataentrypoint', methods=['GET', 'POST'])
+@app.route('/dataentrypoint', methods=['GET'])
 def getfulldata():
-    try:
-       pass
-    except:
-        return "Please send a request Parameter "
+        username=request.args.get("username")
+        anemia=request.args.get("anemia")
+        diabetes=request.args.get("diabetes")
+        bloodpressure=request.args.get("bloodpressure")
+        gender=request.args.get("gender")
+        message=databaseentry(username,anemia,diabetes,bloodpressure,gender)
+        return message
 
 if __name__ == '__main__':
     app.debug = True
--- functions.py
+++ functions.py
@@ -14,7 +14,24 @@ def databasefetcher(username):
     )
     cursor = db.cursor()
     sql = "SELECT * FROM healthcareai WHERE username = %s"
-    usr = (username, )
-    cursor.execute(sql, usr)
+    val = (username, )
+    cursor.execute(sql, val)
     result = cursor.fetchall()
     return result[0]
+
+def databaseentry(username,anemia,diabetes,bloodpressure,gender):
+    db = mysql.connector.connect(
+        host="remotemysql.com",
+        user="hwW4R6cA0s",
+        passwd="9bVe4xsxvX",
+        db="hwW4R6cA0s"
+    )
+    cursor = db.cursor()
+    sql = "INSERT INTO healthcareaiinput VALUES (%s,%s,%s,%s,%s)"
+    val = (username,anemia,diabetes,bloodpressure,gender)
+    try:
+        cursor.execute(sql, val)
+        db.commit()
+        return "successful"
+    except:
+        return "unsuccessful"
