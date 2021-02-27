5bcd3d7065e817ff5d405e5f1a5d76076011384d
Mayank Jha <jhamayank159@gmail.com>
2021-02-28 00:19:51 +0530

    Updated symptom checker endpoints

--- functions.py
+++ functions.py
@@ -1,11 +1,6 @@
 import mysql.connector
 
-def make_data(username):
-    params = dict()
-    (params['username'],params['heartfailpercent'],params['heartfailsevierity'])=databasefetcher(username)
-    return params
-
-def databasefetcher(username):
+def heartandstrokeprediction(username):
     db = mysql.connector.connect(
         host="remotemysql.com",
         user="hwW4R6cA0s",
@@ -13,13 +8,15 @@ def databasefetcher(username):
         db="hwW4R6cA0s"
     )
     cursor = db.cursor()
-    sql = "SELECT * FROM healthcareai WHERE username = %s"
+    sql = "SELECT * FROM heartandstrokeprediction WHERE username = %s"
     val = (username, )
     cursor.execute(sql, val)
     result = cursor.fetchall()
-    return result[0]
+    params = dict()
+    (params['username'],params['heartfail'],params['stroke'])=result[0]
+    return params
 
-def databaseentry(username,anemia,diabetes,bloodpressure,gender):
+def diseaseprediction(username):
     db = mysql.connector.connect(
         host="remotemysql.com",
         user="hwW4R6cA0s",
@@ -27,8 +24,41 @@ def databaseentry(username,anemia,diabetes,bloodpressure,gender):
         db="hwW4R6cA0s"
     )
     cursor = db.cursor()
-    sql = "INSERT INTO healthcareaiinput VALUES (%s,%s,%s,%s,%s)"
-    val = (username,anemia,diabetes,bloodpressure,gender)
+    sql = "SELECT * FROM diseaseprediction WHERE username = %s"
+    val = (username, )
+    cursor.execute(sql, val)
+    result = cursor.fetchall()
+    params = dict()
+    (params['username'],params['diseases'])=result[0]
+    return params
+
+def heartandstrokedatabaseentry(username,age,height,weight,gender,bloodpressure,diabetes,smoking,anaemia,lastcheckup,hypertension,heartdisease,married,worktype,residencetype,glucoselevel):
+    db = mysql.connector.connect(
+        host="remotemysql.com",
+        user="hwW4R6cA0s",
+        passwd="9bVe4xsxvX",
+        db="hwW4R6cA0s"
+    )
+    cursor = db.cursor()
+    sql = "INSERT INTO heartandstroke VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    val = (username,age,height,weight,gender,bloodpressure,diabetes,smoking,anaemia,lastcheckup,hypertension,heartdisease,married,worktype,residencetype,glucoselevel)
+    try:
+        cursor.execute(sql, val)
+        db.commit()
+        return "successful"
+    except:
+        return "unsuccessful"
+
+def symptomsdatabaseentry(username,itching,skin_rash,nodal_skin_eruptions,continuous_sneezing,shivering,chills,joint_pain,stomach_pain,acidity,ulcers_on_tongue,muscle_wasting,vomiting,burning_micturition,spotting_urination,fatigue,weight_gain,anxiety,cold_hands_and_feets,mood_swings,weight_loss,restlessness,lethargy,patches_in_throat,irregular_sugar_level,cough,high_fever,sunken_eyes,breathlessness,sweating,dehydration,indigestion,headache,yellowish_skin,dark_urine,nausea,loss_of_appetite,pain_behind_the_eyes,back_pain,constipation,abdominal_pain,diarrhoea,mild_fever,yellow_urine,yellowing_of_eyes,acute_liver_failure,fluid_overload,swelling_of_stomach,swelled_lymph_nodes,malaise,blurred_and_distorted_vision,phlegm,throat_irritation,redness_of_eyes,sinus_pressure,runny_nose,congestion,chest_pain,weakness_in_limbs,fast_heart_rate,pain_during_bowel_movements,pain_in_anal_region,bloody_stool,irritation_in_anus,neck_pain,dizziness,cramps,bruising,obesity,swollen_legs,swollen_blood_vessels,puffy_face_and_eyes,enlarged_thyroid,brittle_nails,swollen_extremeties,excessive_hunger,extra_marital_contacts,drying_and_tingling_lips,slurred_speech,knee_pain,hip_joint_pain,muscle_weakness,stiff_neck,swelling_joints,movement_stiffness,spinning_movements,loss_of_balance,unsteadiness,weakness_of_one_body_side,loss_of_smell,bladder_discomfort,foul_smell_ofurine,continuous_feel_of_urine,passage_of_gases,internal_itching,toxic_look_typhos,depression,irritability,muscle_pain,altered_sensorium,red_spots_over_body,belly_pain,abnormal_menstruation,dischromic_patches,watering_from_eyes,increased_appetite,polyuria,family_history,mucoid_sputum,rusty_sputum,lack_of_concentration,visual_disturbances,receiving_blood_transfusion,receiving_unsterile_injections,coma,stomach_bleeding,distention_of_abdomen,history_of_alcohol_consumption,fluid_overload_one,blood_in_sputum,prominent_veins_on_calf,palpitations,painful_walking,pus_filled_pimples,blackheads,scurring,skin_peeling,silver_like_dusting,small_dents_in_nails,inflammatory_nails,blister,red_sore_around_nose,yellow_crust_ooze):
+    db = mysql.connector.connect(
+        host="remotemysql.com",
+        user="hwW4R6cA0s",
+        passwd="9bVe4xsxvX",
+        db="hwW4R6cA0s"
+    )
+    cursor = db.cursor()
+    sql = "INSERT INTO symptoms VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    val = (username,itching,skin_rash,nodal_skin_eruptions,continuous_sneezing,shivering,chills,joint_pain,stomach_pain,acidity,ulcers_on_tongue,muscle_wasting,vomiting,burning_micturition,spotting_urination,fatigue,weight_gain,anxiety,cold_hands_and_feets,mood_swings,weight_loss,restlessness,lethargy,patches_in_throat,irregular_sugar_level,cough,high_fever,sunken_eyes,breathlessness,sweating,dehydration,indigestion,headache,yellowish_skin,dark_urine,nausea,loss_of_appetite,pain_behind_the_eyes,back_pain,constipation,abdominal_pain,diarrhoea,mild_fever,yellow_urine,yellowing_of_eyes,acute_liver_failure,fluid_overload,swelling_of_stomach,swelled_lymph_nodes,malaise,blurred_and_distorted_vision,phlegm,throat_irritation,redness_of_eyes,sinus_pressure,runny_nose,congestion,chest_pain,weakness_in_limbs,fast_heart_rate,pain_during_bowel_movements,pain_in_anal_region,bloody_stool,irritation_in_anus,neck_pain,dizziness,cramps,bruising,obesity,swollen_legs,swollen_blood_vessels,puffy_face_and_eyes,enlarged_thyroid,brittle_nails,swollen_extremeties,excessive_hunger,extra_marital_contacts,drying_and_tingling_lips,slurred_speech,knee_pain,hip_joint_pain,muscle_weakness,stiff_neck,swelling_joints,movement_stiffness,spinning_movements,loss_of_balance,unsteadiness,weakness_of_one_body_side,loss_of_smell,bladder_discomfort,foul_smell_ofurine,continuous_feel_of_urine,passage_of_gases,internal_itching,toxic_look_typhos,depression,irritability,muscle_pain,altered_sensorium,red_spots_over_body,belly_pain,abnormal_menstruation,dischromic_patches,watering_from_eyes,increased_appetite,polyuria,family_history,mucoid_sputum,rusty_sputum,lack_of_concentration,visual_disturbances,receiving_blood_transfusion,receiving_unsterile_injections,coma,stomach_bleeding,distention_of_abdomen,history_of_alcohol_consumption,fluid_overload_one,blood_in_sputum,prominent_veins_on_calf,palpitations,painful_walking,pus_filled_pimples,blackheads,scurring,skin_peeling,silver_like_dusting,small_dents_in_nails,inflammatory_nails,blister,red_sore_around_nose,yellow_crust_ooze)
     try:
         cursor.execute(sql, val)
         db.commit()
